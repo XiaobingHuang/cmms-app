1b12b4bde2ef9aa07269dd2d3d502b79d810e9b0
Jeff Huang <jeff.huang@spatialfront.com>
2025-09-15 19:50:03 -0400

    login

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-50 text-gray-900">
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif;
+  background-color: #f9fafb;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,8 +2,12 @@
 </script>
 
 <template>
-    <div>
-        LoginView
+    <div class="min-h-screen flex items-center justfy-center bg-gray-50">
+        <div class="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
+            <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">
+                CMMS
+            </h1>
+        </div>
     </div>
 
 </template>
