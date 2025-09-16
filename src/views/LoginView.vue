5400db028c91b9ddbe8a76f9fad7b3af00f3e352
Jeff Huang <jeff.huang@spatialfront.com>
2025-09-16 18:34:21 -0400

    navigation bar

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,37 +1,70 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {useRouter} from "vue-router";
+import {computed, ref} from "vue";
+import {useLink, useRouter} from "vue-router";
 import { useUserStore } from "../store/UserStore";
+import {isEmail} from "../untilis/unitilisFunctions"
+
 
 const userName = ref("");
 const password = ref("");
-const error = ref("");
+const error = ref({
+    name:"",
+    password:""
+});
 
 const router = useRouter();
 const userStore = useUserStore();
 
 function handleLogin(){
-    const success = userStore.login(userName.value, password.value);
-    if(success){
-        router.push("/dashboard")
-    } else {
-        error.value = "Invalid username or password";
+    const isUserName = validateUserName(userName.value);
+    const ispassword = validatePassword(password.value);
+
+    if(isUserName && ispassword){
+        const success = userStore.login(userName.value, password.value);
+        if(success){
+            router.push("/dashboard")
+        } else {
+            // error information center ...
+            error.value.name = "Invalid username or password";
+        }
     }
+}
+
+function validateUserName(input: string):boolean{
+    if(input.trim() === "") {
+        error.value.name = "The email address field is required";
+        return false
+    }
+    if(!isEmail(input)) {
+        error.value.name = "The email address field must be a valid email";
+        return false;
+    }
+    return true
+}
+
+function validatePassword(input:string):boolean{
+    if(input.trim() === "") {
+        error.value.password = "The email address field is required";
+        return false
+    }
+    return true
 }
 
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justfy-center bg-gray-50">
-        <div class="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
-            <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">
-                CMMS
+    <div class="min-h-screen flex items-center justify-center bg-gray-50">
+        <div class="w-full max-w-md bg-white p-8 rounded">
+            <h1 class="text-3xl font-bold text-center mb-6 helix-text">
+                <strong>Helix</strong>Intel
             </h1>
             <form @submit.prevent="handleLogin">
-                <input v-model="userName" placeholder="Enter your name" class="input"></input>
+                <input v-model="userName" placeholder="Email Address" class="input"></input>
+                <p v-if="error.name" class="text-red-600 mb-4" >{{ error.name }}</p>
                 <input v-model="password" placeholder="Password" class="input" type="password"></input>
-                <button type="submit" class="btn"></button>
-                <p v-if="error" class="text-red-600 mt-2" >{{ error }}</p>
+                <p v-if="error.name" class="text-red-600 mb-4" >{{ error.password }}</p>
+                <button type="submit" class="btn">Log In</button>
+                
 
             </form>
 
@@ -50,12 +83,15 @@ function handleLogin(){
   border-radius: 0.5rem;
 }
 .btn {
-  background: #42b983;
+  background: #205f92;
   color: white;
   font-weight: bold;
   padding: 0.5rem;
   border-radius: 0.5rem;
   width: 100%;
+}
+.helix-text {
+  color: var(--color-deep-blue);
 }
 
 </style>
