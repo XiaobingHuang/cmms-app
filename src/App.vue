5a9ea93f305ec1a3b63d7163f89dfb3b59c82382
Jeff Huang <jeff.huang@spatialfront.com>
2025-09-16 21:47:58 -0400

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import AppLayout from './components/AppLayout.vue';
+
+const route = useRoute();
+
+const shouldShowLayout = computed(() => {
+  return route.meta.requiresAuth === true;
+});
 </script>
 
 <template>
-  <AppLayout>
+  <AppLayout v-if="shouldShowLayout">
     <router-view />
   </AppLayout>
+  <router-view v-else />
 </template>
 
 <style scoped>
