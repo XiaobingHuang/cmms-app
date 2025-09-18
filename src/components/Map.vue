203009a3be1e61ac8ea84daabad16afcdb36fe9f
Jeff Huang <jeff.huang@spatialfront.com>
2025-09-17 20:59:09 -0400

    map added

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { Loader } from "@googlemaps/js-api-loader";
+
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string;
+
+const mapElement = ref<HTMLElement | null>(null);
+
+onMounted(async () => {
+  const loader = new Loader({
+    apiKey, 
+    version: "weekly",
+  });
+
+  const google = await loader.load();
+
+  const map = new google.maps.Map(mapElement.value as HTMLElement, {
+    center: { lat: 40.7128, lng: -74.006 },
+    zoom: 12,
+  });
+
+  new google.maps.Marker({
+    position: { lat: 40.7128, lng: -74.006 },
+    map,
+    title: "Hello NYC!",
+  });
+});
+</script>
+
+<template>
+  <div ref="mapElement" class="w-full h-[400px] rounded-lg shadow"></div>
+</template>
